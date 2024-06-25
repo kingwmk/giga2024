0b6635583c8df7a3e1ae1d8a4a13ce28b5f1906a
kingwmk <1171147450@qq.com>
2024-06-25 20:57:58 +0800

    Create __init__.py

---
+++ Transformer/losses/__init__.py
@@ -0,0 +1,7 @@
+
+
+from losses.gaussian_nll_loss import GaussianNLLLoss
+from losses.laplace_nll_loss import LaplaceNLLLoss
+from losses.mixture_nll_loss import MixtureNLLLoss
+from losses.nll_loss import NLLLoss
+from losses.von_mises_nll_loss import VonMisesNLLLoss
