52811e99f070d31fd3d8be03b78a573ce2f06994
kingwmk <1171147450@qq.com>
2024-06-25 21:01:01 +0800

    Update giganet.py

--- Transformer/giganet.py
+++ Transformer/giganet.py
@@ -11,10 +11,10 @@ import torch.nn.functional as F
 from torch_geometric.data import Batch
 from torch_geometric.data import HeteroData
 
-import MixtureNLLLoss
-import NLLLoss
-import minADE
-import minFDE
+from losses import MixtureNLLLoss
+from losses import NLLLoss
+from metrics import minADE
+from metrics import minFDE
 import GigaNetDecoder
 import GigaNetEncoder
 
