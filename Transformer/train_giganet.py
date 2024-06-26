b92b2eb584e506d69375d33edb017fdcdb90a58e
kingwmk <1171147450@qq.com>
2024-06-26 13:55:56 +0800

    Update train_giganet.py

--- Transformer/train_giganet.py
+++ Transformer/train_giganet.py
@@ -1,17 +1,3 @@
-# Copyright (c) 2023, Zikang Zhou. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3,4,5,6,7'
 import shutil
@@ -22,15 +8,13 @@ from pytorch_lightning.callbacks import LearningRateMonitor
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.strategies import DDPStrategy
 
-from datamodules import ArgoverseV2DataModule
-from datamodules import ArgoverseV1DataModule
-from predictors import QCNet
+from datamodules import GigaDataModule
+import GigaNet
 
 if __name__ == '__main__':
     pl.seed_everything(2024, workers=True)
 
     parser = ArgumentParser()
-    parser.add_argument('--root', type=str, default='/mnt/home/data/argoverse2/motion')
     parser.add_argument('--resume', default="", type=str, metavar="RESUME", help="checkpoint path")
     parser.add_argument('--train_batch_size', type=int, default=5)
     parser.add_argument('--val_batch_size', type=int, default=4)
@@ -39,21 +23,18 @@ if __name__ == '__main__':
     parser.add_argument('--num_workers', type=int, default=7)
     parser.add_argument('--pin_memory', type=bool, default=True)
     parser.add_argument('--persistent_workers', type=bool, default=True)
-    parser.add_argument('--train_raw_dir', type=str, default=None)
-    parser.add_argument('--val_raw_dir', type=str, default=None)
-    parser.add_argument('--test_raw_dir', type=str, default=None)
     parser.add_argument('--train_processed_dir', type=str, default=None)
     parser.add_argument('--val_processed_dir', type=str, default=None)
     parser.add_argument('--test_processed_dir', type=str, default=None)
     parser.add_argument('--accelerator', type=str, default='auto')
-    parser.add_argument('--devices', type=int, default=7)
+    parser.add_argument('--devices', type=int, default=8)
     parser.add_argument('--max_epochs', type=int, default=64)
     parser.add_argument('--log_path', type=str, default='./log/test')
-    QCNet.add_model_specific_args(parser)
+    GigaNet.add_model_specific_args(parser)
     args = parser.parse_args()
-    model = QCNet(**vars(args))
-    src_dirs = ["./", "./modules", "./predictors", "./datasets"]
-    dst_dirs = [args.log_path, args.log_path+"/modules", args.log_path+"/predictors", args.log_path+"/datasets"]
+    model = GigaNet(**vars(args))
+    src_dirs = ["./",]
+    dst_dirs = [args.log_path,]
     for src_dir, dst_dir in zip(src_dirs, dst_dirs):
         files = [f for f in os.listdir(src_dir) if f.endswith(".py")]
         if not os.path.exists(dst_dir):
@@ -61,11 +42,8 @@ if __name__ == '__main__':
         for f in files:
             shutil.copy(os.path.join(src_dir, f), os.path.join(dst_dir, f))
             
-    datamodule = {
-        'argoverse_v2': ArgoverseV2DataModule,
-        'argoverse_v1': ArgoverseV1DataModule,
-    }[args.dataset](**vars(args))
-    model_checkpoint = ModelCheckpoint(monitor='val_minFDE', save_top_k=6, mode='min')
+    datamodule = GigaDataModule(**vars(args))
+    model_checkpoint = ModelCheckpoint(monitor='val_minFDE', save_top_k=20, mode='min')
     lr_monitor = LearningRateMonitor(logging_interval='epoch')
     trainer = pl.Trainer(accelerator=args.accelerator, devices=args.devices,
             strategy=DDPStrategy(find_unused_parameters=False, gradient_as_bucket_view=True),
