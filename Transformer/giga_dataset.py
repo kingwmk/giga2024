40313f58de8ab37180ae571b1bc1c9ee9ee5a9b8
kingwmk <1171147450@qq.com>
2024-06-28 21:26:36 +0800

    Update giga_dataset.py

--- Transformer/giga_dataset.py
+++ Transformer/giga_dataset.py
@@ -4,15 +4,23 @@ import pickle
 
 class GigaDataset(Dataset):
     def __init__(self, processed_dir, transform = None) -> None:
-        self.processed_dir = processed_dir
-        self.processed_file_names = [name for name in os.listdir(processed_dir)]
+        self._processed_dir = processed_dir
+        self._processed_file_names = [name for name in os.listdir(processed_dir)]
         super(GigaDataset, self).__init__(processed_dir=processed_dir, transform=transform)
-    
+        
+    @property
+    def processed_dir(self):
+        return self._processed_dir
+
+    @processed_dir.setter
+    def processed_dir(self, value):
+        self._processed_dir = value    
+        
     def len(self) -> int:
-        return len(self.processed_file_names)
+        return len(self._processed_file_names)
 
     def get(self, idx: int):
-        file = self.processed_dir + self.processed_file_names[idx]
+        file = self._processed_dir + self._processed_file_names[idx]
         with open(file, 'rb') as handle:
             return HeteroData(pickle.load(handle))
 
