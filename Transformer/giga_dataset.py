ee63d6f11a673a9edf3e732584eb2197487da269
kingwmk <1171147450@qq.com>
2024-06-28 21:15:34 +0800

    Update giga_dataset.py

--- Transformer/giga_dataset.py
+++ Transformer/giga_dataset.py
@@ -6,7 +6,7 @@ class GigaDataset(Dataset):
     def __init__(self, processed_dir, transform = None) -> None:
         self.processed_dir = processed_dir
         self.processed_file_names = [name for name in os.listdir(processed_dir)]
-        super(GigaDataset, self).__init__(transform=transform)
+        super(GigaDataset, self).__init__(processed_dir=processed_dir, transform=transform)
     
     def len(self) -> int:
         return len(self.processed_file_names)
