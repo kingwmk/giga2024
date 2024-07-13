cb05d54c378b6ab0ae1d838e10e4b38a6d6779df
kingwmk <1171147450@qq.com>
2024-07-13 20:57:57 +0800

    Update test_ensembling_mean_test.py

--- FPN_CNN/test_ensembling_mean_test.py
+++ FPN_CNN/test_ensembling_mean_test.py
@@ -289,7 +289,7 @@ def main():
                     kmeans = KMeans(n_clusters=3, n_init='auto', random_state=0).fit(traj_ends)
                     cts = kmeans.cluster_centers_
                     result = np.zeros((3, 60, 2), np.float64)
-                    orign = gt_past[59:60]
+                    orign = gt_pasts[i][59:60]
                     for j in range(3):  
                         vel = (cts[j] - orign)/60
                         vel_pred = np.repeat(vel, 60, axis=0)
