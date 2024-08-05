b90c689238009932cf1ac97ee6724bc4d3b73d54
kingwmk <1171147450@qq.com>
2024-08-05 23:24:31 +0800

    Update test_orch.sh

--- FPN_CNN/test_orch.sh
+++ FPN_CNN/test_orch.sh
@@ -1,22 +1,7 @@
-python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 3.812.ckpt --result_path 3.812
-python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 3.611.ckpt --result_path 3.611
-python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 3.411.ckpt --result_path 3.411
-python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 3.210.ckpt --result_path 3.210
-python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 3.009.ckpt --result_path 3.009
-python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 2.809.ckpt --result_path 2.809
-python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 2.608.ckpt --result_path 2.608
-python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 2.407.ckpt --result_path 2.407
-python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 2.207.ckpt --result_path 2.207
-python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 2.006.ckpt --result_path 2.006
-python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 1.806.ckpt --result_path 1.806
-python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 1.605.ckpt --result_path 1.605
-python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 1.404.ckpt --result_path 1.404
-python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 1.204.ckpt --result_path 1.204
-python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 1.003.ckpt --result_path 1.003
-python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 0.802.ckpt --result_path 0.802
-python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 0.602.ckpt --result_path 0.602
-
-
+python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 0.401.ckpt --result_path 0.401
+python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 0.201.ckpt --result_path 0.201
+python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 2.307.ckpt --result_path 2.307
+python test_gaussian_moving_average_vis_left9v1.py -m1 gigaNet_stride_6_test_rand_dcms3_train --weight1 2.508.ckpt --result_path 2.508
 
 
 
